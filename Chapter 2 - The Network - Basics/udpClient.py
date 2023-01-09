91b83627b9fd13c354c7fbc9d4079649a32cd900
Mohamad-Alhamwi <moda.tnt.edu@outlook.com>
2023-01-09 17:18:35 +0300

    Added udpClient.py.

---
+++ Chapter 2 - The Network - Basics/udpClient.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import socket
+import sys
+
+target_host = "127.0.0.1"
+target_port = 80
+
+try:
+    # Create a UDP-socket object.
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+except socket.error as error_message: 
+    # Print out the error message.
+    print (f"Error creating socket: {error_message}") 
+
+    # Unsuccessful termination.
+    sys.exit(1) 
+
+try:
+    # Send the server some data.
+    client.sendto("AAABBBCCC".encode(), (target_host, target_port))
+
+except socket.error as error_message:  
+    # Print out the error message.
+    print (f"Error sending data: {error_message}") 
+    
+    # Unsuccessful termination.
+    sys.exit(1) 
+
+try:
+    # Receive both the data and the details of the remote host and port.
+    data, add = client.recvfrom(4096)
+
+except socket.error as error_message:  
+    # Print out the error message.
+    print (f"Error receiving data: {error_message}") 
+    
+    # Unsuccessful termination.
+    sys.exit(1)
+
+# Print out the data.
+print(data)
